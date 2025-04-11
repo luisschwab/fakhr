5a7cab1976bb1fc0b8b5d494fe90aba544a2e858
Luis Schwab <luisschwab@protonmail.com>
2025-04-11 17:58:13 -0300

    feat: nostr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//! Fakhr (فخر - meaning pride in arabic) is a Bitcoin vanity address generator.
+//! Fakhr (فخر - meaning pride in arabic) is a vanity Bitcoin address and Nostr key generator.
 //!
 //! Supports all Bitcoin script types and networks.
 //!
@@ -17,15 +17,17 @@ use bitcoin::secp256k1::{Keypair, Secp256k1, rand};
 use bitcoin::{Address, AddressType, CompressedPublicKey, Network, PrivateKey, PublicKey, Script};
 use clap::builder::PossibleValuesParser;
 use clap::{Parser, command};
+use nostr::nips::nip19::ToBech32;
+use nostr::Keys;
 use num_format::{Locale, ToFormattedString};
 
 const BASE58_SET: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const BECH32_SET: &str = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
 
 #[derive(Parser, Debug)]
-#[command(author="luisschwab", version, about="Fakhr (فخر - meaning pride in arabic) is a Bitcoin vanity address generator.", long_about = None)]
+#[command(author="luisschwab", version, about="Fakhr (فخر - meaning pride in arabic) is a vanity Bitcoin address and Nostr key generator.", long_about = None)]
 struct Args {
-    #[arg(short, long, value_parser=PossibleValuesParser::new(["1", "3", "bc1q", "bc1p", "m", "n", "2", "tb1q", "tb1p",]))]
+    #[arg(short, long, value_parser=PossibleValuesParser::new(["1", "3", "bc1q", "bc1p", "m", "n", "2", "tb1q", "tb1p", "npub1"]))]
     prefix: String,
 
     #[arg(short, long)]
@@ -34,7 +36,7 @@ struct Args {
 
 /// This checks that the suffix only contains characters present in the charset defined by the
 /// prefix, and returns the corresponding ([`AddressType`], [`Network`]) tuple if there are no conflicts.
-fn parse_prefix_suffix(prefix: String, suffix: String) -> Result<(AddressType, Network)> {
+fn parse_bitcoin_prefix_suffix(prefix: String, suffix: String) -> Result<(AddressType, Network)> {
     // Assemble address type and network tuples
     let (address_type, network) = match prefix.as_str() {
         // Mainnet
@@ -73,9 +75,9 @@ fn parse_prefix_suffix(prefix: String, suffix: String) -> Result<(AddressType, N
 }
 
 /// Mine a Bitcoin address with a given suffix.
-fn mine(prefix: String, suffix: String, address_type: AddressType, network: Network) {
+fn mine_bitcoin(prefix: String, suffix: String, address_type: AddressType, network: Network) {
     let mined = Arc::new(AtomicBool::new(false));
-    let n_threads = num_cpus::get() - 2;
+    let n_threads = num_cpus::get() - 2; // leave some for the rest of us
     let (sender, receiver) = mpsc::channel::<(String, String, String)>();
 
     let mut handles = vec![];
@@ -83,7 +85,7 @@ fn mine(prefix: String, suffix: String, address_type: AddressType, network: Netw
     let mut total_iterations: u128 = 0;
 
     let start = Instant::now();
-    for thread_id in 0..n_threads {
+    for _ in 0..n_threads {
         let prefix = prefix.clone();
         let suffix = suffix.clone();
         let mined = mined.clone();
@@ -129,7 +131,7 @@ fn mine(prefix: String, suffix: String, address_type: AddressType, network: Netw
 
                 // Log every 1000 addresses so mining is not bottenecked by I/O
                 if iter % 1000 == 0 {
-                    println!("[{}] {}", thread_id, address.to_string());
+                    println!("{}", address.to_string());
                 }
 
                 // Check if address contains the suffix at the beginning.
@@ -144,10 +146,6 @@ fn mine(prefix: String, suffix: String, address_type: AddressType, network: Netw
 
                         break;
                     }
-                }
-
-                if iter % 1000 == 0 {
-                    thread::yield_now();
                 }
 
                 iter += 1;
@@ -165,8 +163,6 @@ fn mine(prefix: String, suffix: String, address_type: AddressType, network: Netw
 
     match receiver.recv() {
         Ok((address_string, secret_key, wif)) => {
-            mined.store(true, Ordering::Relaxed);
-
             for handle in handles {
                 if let Ok(iterations) = handle.join() {
                     total_iterations += iterations;
@@ -192,15 +188,112 @@ fn mine(prefix: String, suffix: String, address_type: AddressType, network: Netw
     }
 }
 
+/// This checks that the suffix only contains characters present in the BECH32_SET.
+fn parse_nostr_suffix(suffix: String) -> Result<()> {
+    if let Some(invalid) = suffix.chars().find(|&c| !BECH32_SET.contains(c)) {
+        return Err(anyhow!("Invalid character for bech32 charset: {}", invalid));
+    }
+
+    Ok(())
+}
+
+// Mine a nostr pubkey with a given suffix.
+fn mine_nostr(prefix: String, suffix: String) {
+    let mined = Arc::new(AtomicBool::new(false));
+    let n_threads = num_cpus::get() - 2; // leave some for the rest of us
+    let (sender, receiver) = mpsc::channel::<(String, String)>();
+
+    let mut handles = vec![];
+    // Total iterations across all threads
+    let mut total_iterations: u128 = 0;
+
+    let start = Instant::now();
+    for _ in 0..n_threads {
+        let prefix = prefix.clone();
+        let suffix = suffix.clone();
+        let mined = mined.clone();
+        let sender = sender.clone();
+
+        let handle = thread::spawn(move || {
+            let mut iter: u128 = 0;
+
+            while !mined.load(Ordering::Relaxed) {
+                let keys = Keys::generate();
+
+                let pubkey = keys.public_key().to_bech32().expect("failed to get bech32-encoded nostr pubkey");
+                let pubkey_clone = pubkey.clone();
+
+                // Log every 1000 keys so mining is not bottenecked by I/O
+                if iter % 1000 == 0 {
+                    println!("{}", pubkey.to_string());
+                }
+
+                let sans_prefix = pubkey_clone.strip_prefix(&prefix).expect("failed to strip prefix from bech32-encoded nostr pubkey");
+
+                if sans_prefix.starts_with(&suffix) {
+                    let privkey = keys.secret_key().to_bech32().expect("failed to get bech32-encoded nostr privkey");
+
+                    mined.store(true, Ordering::Relaxed);
+
+                    sender.send((pubkey, privkey)).expect("sender failed to send");
+
+                    break;
+                }
+
+                iter += 1;
+            }
+
+            iter
+        });
+
+        handles.push(handle);
+    }
+
+    // Drop the original sender so
+    // receiver.recv() knows that the sender is terminated
+    drop(sender);
+
+    match receiver.recv() {
+        Ok((pubkey, privkey)) => {
+            for handle in handles {
+                if let Ok(iterations) = handle.join() {
+                    total_iterations += iterations;
+                }
+            }
+
+            let elapsed = start.elapsed().as_secs() as u128 | 1;
+
+            println!(
+                "\nFound {} in {:02}:{:02}:{:02} and {} iterations ({} iter/s)",
+                pubkey,
+                elapsed / 3600,
+                elapsed % 3600 / 60,
+                elapsed % 60,
+                total_iterations.to_formatted_string(&Locale::en),
+                (total_iterations / elapsed as u128).to_formatted_string(&Locale::en)
+            );
+            println!("SecretKey: {}", privkey);
+        },
+
+        Err(_) => panic!("threads were terminated before finding a match"),
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let prefix = args.prefix;
     let suffix = args.suffix;
 
-    let (address_type, network) = parse_prefix_suffix(prefix.clone(), suffix.clone())?;
+    if prefix == "npub1" {
+        parse_nostr_suffix(suffix.clone())?;
 
-    mine(prefix, suffix, address_type, network);
+        mine_nostr(prefix.clone(), suffix.clone());
+    } else {
+        let (address_type, network) = parse_bitcoin_prefix_suffix(prefix.clone(), suffix.clone())?;
+
+        mine_bitcoin(prefix, suffix, address_type, network);
+    }
 
     Ok(())
 }
